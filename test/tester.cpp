66fc5c77dbe6cd7c48ccae1917f40f4e3d611a04
John Fusco <nyrinwi@gmail.com>
2019-05-27 13:54:45 -0500

    modified:   test/tester.cpp

--- test/tester.cpp
+++ test/tester.cpp
@@ -8,6 +8,7 @@
 class Tempfile
 {
     std::string m_filename;
+    const size_t m_size;
     int m_fd;
 public:
     const char* filename() const
@@ -16,11 +17,14 @@ public:
     int fd() const
         { return m_fd; }
 
+    void read(ssize_t nbytes=-1);
+
     Tempfile(size_t size=0);
     ~Tempfile();
 };
 
 Tempfile::Tempfile(size_t size)
+: m_size(size)
 {
     char name[] = "tempfile.XXXXXX";
     m_fd = mkstemp(name);
@@ -33,12 +37,44 @@ Tempfile::Tempfile(size_t size)
     }
 }
 
+void Tempfile::read(ssize_t nbytes)
+{
+    if ( nbytes == -1 )
+    {
+        nbytes = m_size;
+    }
+    char buf[ getpagesize() ];
+
+    int nwrites = (nbytes + getpagesize() - 1) / getpagesize();
+    lseek(m_fd,0,SEEK_SET);
+    for (int i=0; i<nwrites; i++)
+    {
+        ssize_t rsize = getpagesize();
+        ssize_t readto = (i+1)*rsize;;
+        if ( readto > m_size )
+        {
+            rsize = m_size % getpagesize();
+        }
+        ssize_t r = ::read(m_fd,buf,rsize);
+        if ( r == -1 )
+        {
+            perror("read");
+        }
+        if ( r != rsize )
+        {
+            std::cerr << "rsize " << rsize << std::endl;
+            std::cerr << "r " << r << std::endl;
+        }
+        assert(r == rsize);
+    }
+}
+
 Tempfile::~Tempfile()
 {
     unlink(filename());
 }
 
-TEST(mapping,basic)
+TEST(mapping,mincore)
 {
     const size_t npages = 128;
     const size_t filesize = npages*getpagesize();
@@ -52,13 +88,12 @@ TEST(mapping,basic)
 TEST(mapping,evict)
 {
     const size_t npages = 128;
-    const size_t filesize = npages*getpagesize();
+    const size_t filesize = npages*getpagesize() - getpagesize()/2;
 
     Tempfile tmp(filesize);
     Mapping* mapping = new Mapping(tmp.filename());
 
-    char* buf = new char[filesize];
-    memcpy(buf,mapping->ptr(),filesize);
+    tmp.read(filesize);
     mapping->mincore();
     EXPECT_EQ(mapping->n_resident,npages);
 
